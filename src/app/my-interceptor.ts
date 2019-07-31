52c52834774f935d4a742dc1f7a6f97f0acb4c8c
Kevin <kevin.kuehle@googlemail.com>
2019-07-31 14:49:50 +0200

    chage search html

--- src/app/my-interceptor.ts
+++ src/app/my-interceptor.ts
@@ -1,6 +1,7 @@
 import { Observable } from 'rxjs';
 import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { tap } from 'rxjs/operators';
 
 @Injectable()
 export class MyInterceptor implements HttpInterceptor {
@@ -11,7 +12,9 @@ export class MyInterceptor implements HttpInterceptor {
     const response$ = next.handle(request);
 
     return response$.pipe(
+      tap( term => console.log('Hi ich bims ein HttpInterceptor! :)') )
       // HTTP-Response verarbeiten
+
     );
   }
 }
